6bdcdd869d6791697dd6e6e0f928d7bd5a9957b7
Kirsten Kwong <kskwong@ualberta.ca>
2018-10-17 16:00:36 -0600

    styling

--- webresume/webresume/settings.py
+++ webresume/webresume/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
